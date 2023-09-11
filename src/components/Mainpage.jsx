3e5f43642c6698c1ee8a78b4f436ea424f621a6a
Rheanne Bande <rheanne.bande@gmail.com>
2023-09-10 23:21:01 -0700

    build about section amd style component

--- src/components/Mainpage.jsx
+++ src/components/Mainpage.jsx
@@ -1,9 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import RBGLogo from '../assets/images/RBG-login-page.png';
+import RBGicon from '../assets/images/gamepad-logo.png';
 import Wishfor from '../assets/images/wishfor-landing.png';
 import waveEmoji from '../assets/images/wave-emoji.png';
 import chatBubble from '../assets/images/chat_emoji.png';
+import personalImg from '../assets/images/personal-image.jpg';
 import { Link } from 'react-router-dom';
 
 const Main = () => {
@@ -16,20 +18,20 @@ const Main = () => {
                     </div>
                     <div>
                         <ul className="d-flex flex-row mt-3">
-                            <li className='nav-list'>About</li>
-                            <li className='nav-list'>Projects</li>
-                            <li className='nav-list'>Contact</li>
+                            <li className='nav-list'><a href='#about' className='nav-btn'>About</a></li>
+                            <li className='nav-list'><a href='#projects' className='nav-btn'>Projects</a></li>
+                            <li className='nav-list'><a href='#contact' className='nav-btn'>Contact</a></li>
                         </ul>
                     </div>
                 </div>
             </nav>
-            <header className='tag'>
+            <header className='mb-4'>
                 <div className='intro-section text-white d-flex flex-column align-items-center'>
                     <div>
                         <h1 className="hello-world">Hello, world!<img src={waveEmoji} alt='wave emoji' className='emoji mx-2' /></h1>
                         <h1 className='name'>I'm Rheanne Bande </h1>
                         <h1 className='intro-3rd-row mb-4' style={{color: "#28104E"}}>I am a <span className='gradient-text'>Software Developer</span></h1>
-                        <p className='small-bio mb-4'>Healthcare transitioned Software Developer based in Bay Area, CA specializing in Front End Development</p>
+                        <p className='small-bio mb-4'>Healthcare transitioned Software Developer based in Bay Area, CA specializing in Front End Web Development</p>
                         <div className='d-flex justify-content-center align-items-center mb-4'>
                             <button className='resume-btn'>Resume</button>
                         </div>
@@ -41,7 +43,7 @@ const Main = () => {
                         </ul>
                     </div>
                     <div className='technologies d-flex flex-column text-center'>
-                        <p className='fs-2 text-white mb-5'>Technologies</p>
+                        <p className='fs-2 text-white mb-5'>Top Technologies</p>
                         <ul className='d-flex flex-row justify-content-evenly'>
                             <li><i class="tech-icon devicon-javascript-plain"></i></li>
                             <li><i class="tech-icon devicon-python-plain"></i></li>
@@ -56,28 +58,45 @@ const Main = () => {
                     </div>
                 </div>
             </header>
-            <section className="tag">
-                <h1 className='section-title'>About</h1>
-                <div className='about-paragraph'>
-                    <p>Hello! My name is Rheanne and I enjoy building websites to showcase to others! I'm a healthcare turned software engineer after I decided to try programming through various online resources. I attended Coding Dojo, a fullstack software engineering bootcamp that helped guide me to kickstart my career in tech. </p>
-                    <p>When I'm not in front of a computer, I enjoy being outdoors, bowling, gaming, or spending time with my friends, family, cats and reptile.</p>
+            <h1 className='section-title text-white mx-4 mb-4' id='about'>About</h1>
+            <section className="about-section">
+                <div className="about-left">
+                    <div className='about-paragraph'>
+                        <p>Hello! My name is Rheanne and I enjoy building websites to showcase to others! I'm a healthcare turned software engineer after deciding to try programming through various online resources.</p>
+                        <p>First attending Coding Dojo, a fullstack software engineering bootcamp, helped guide me with the structure and resources to kickstart my career in tech. Navigating my way to learning new languages and technologies has been a challenging yet-rewarding experience!</p>
+                        <p>When I'm not in front of a computer, I enjoy being outdoors, bowling, gaming, or spending time with my friends, family, cats and reptile.</p>
+                    </div>
+                    <div className='d-flex flex-column align-items-center'>
+                        <p className='tech-header'>Technologies I have worked with</p>
+                        <div className='tech-list d-flex flex-row'>
+                            <ul>
+                                <li>- Javascript </li>
+                                <li>- Python</li>
+                                <li>- Ruby</li>
+                                <li>- MERN</li>
+                                <li>- Git</li>
+                                <li>- Create React App</li>
+                            </ul>
+                            <ul>
+                                <li>- Vite</li>
+                                <li>- Bootstrap</li>
+                                <li>- Postman</li>
+                                <li>- Flask</li>
+                                <li>- MySQL</li>
+                                <li>- Balsamiq</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <p>Technologies I have worked with:</p>
-                    <ul>
-                        <li>- Javascript </li>
-                        <li>- MERN</li>
-                        <li>- Python</li>
-                        <li>- Postman</li>
-                        <li>- Bootstrap</li>
-                    </ul>
+                <div className="about-right">
+                    <img src={personalImg} alt="personal image" className="personal-img" />
                 </div>
             </section>
-            <h1 className='section-title text-white mx-4'>Projects</h1>
+            <h1 className='section-title text-white mx-4' id="projects">Projects</h1>
             <section id="project1">
                 <div className='project-container'>
                     <div className='project-desc-container'>
-                        <h1 className='project-title'>Wishfor_</h1>
+                        <h1 className='project-title' style={{fontFamily:"Borel"}}>Wishfor__</h1>
                         <p>A dark-themed website designed to curate a wishlist of items, organized into one convenient place. Built with React & Create React App</p>
                         <p>Technologies Used:</p>
                         <ul>
@@ -86,7 +105,7 @@ const Main = () => {
                             <li className='mb-3'>+ Bootstrap</li>
                             <li className='mb-5'>+ Balsamiq</li>
                         </ul>
-                        <button className='github-repo-btn'>Github Repo</button>
+                        <Link to={"https://github.com/Rhemb/wishfor__"} target='_blank'><button className='github-repo-btn'>Github Repo</button></Link>
                     </div>
                     <div className='project-1 mt-5 d-flex align-items-center'>
                         <img className="project-feature" src={Wishfor} alt="landing page"/>
@@ -96,7 +115,7 @@ const Main = () => {
             <section id="project2">
                 <div className="project-container">
                     <div className='project-desc-container'>
-                        <h1 className='project-title'>RBG</h1>
+                        <h1 className='project-title d-flex flex-row align-items-center'><img src={RBGicon} alt="gamepad logo" className="rbg-icon me-2"/>RBG</h1>
                         <p>RBG, Reviews By Gamers, a website designed to review video games played by other gamers in the community. Built by a group of 5 with React & Vite.</p>
                         <p>Technologies Used:</p>
                         <ul>
@@ -104,7 +123,7 @@ const Main = () => {
                             <li className='mb-3'>+ MERN</li>
                             <li className='mb-3'>+ Balsamiq</li>
                             <li className='mb-5'>+ Bootstrap</li>
-                            <li><button className='github-repo-btn'>Github Repo</button></li>
+                            <li><Link to={'https://github.com/austinobnillas/RBG-reviews'} target='_blank'><button className='github-repo-btn'>Github Repo</button></Link></li>
                         </ul>
                     </div>
                     <div className='project-2 d-flex align-items-center'>
@@ -112,12 +131,12 @@ const Main = () => {
                     </div>
                 </div>
             </section>
-            <section className='contact-section'>
+            <section className='contact-section' id='contact'>
                 <h1 className='contact-header d-flex align-items-center mb-3'>Lets Chat<img src={chatBubble} alt="chat bubble" className="chat-emoji" /></h1>
                 <p className='contact-text'>Thanks for taking the time to get to know me! Please feel free to reach out through my socials if you're interested in connecting</p>
                 <div className='d-flex flex-row'>
                     <Link to={"https://www.linkedin.com/in/rheanne-bande/"} target='_blank' className='text-decoration-none p-3'><i class="devicon-linkedin-plain contact-icon linkedin"></i></Link>
-                    <Link to={"https://github.com/Rhemb"} target='_blank' className='text-decoration-none p-3'><i class="devicon-github-original contact-icon"></i></Link>
+                    <Link to={"https://github.com/Rhemb"} target='_blank' className='text-decoration-none p-3'><i class="devicon-github-original contact-icon github"></i></Link>
                 </div>
             </section>
         </div>
