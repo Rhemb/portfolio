91cacfd787e4592810a070532533fa9eb81089bd
Rheanne Bande <rheanne.bande@gmail.com>
2023-09-14 17:25:21 -0700

    add an image carousel. style main page with animations.

--- src/components/Mainpage.jsx
+++ src/components/Mainpage.jsx
@@ -1,14 +1,47 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
 import RBGLogo from '../assets/images/RBG-login-page.png';
 import RBGicon from '../assets/images/gamepad-logo.png';
 import Wishfor from '../assets/images/wishfor-landing.png';
 import waveEmoji from '../assets/images/wave-emoji.png';
 import chatBubble from '../assets/images/chat_emoji.png';
 import personalImg from '../assets/images/personal-image.jpg';
+import crochet from '../assets/images/crochet.jpg';
+import sf from '../assets/images/sf.jpg';
+import stella from '../assets/images/stella.jpg';
+
+
+import React, {useEffect, useRef, useState} from 'react';
 import { Link } from 'react-router-dom';
 
 const Main = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [isVisible, setVisible] = useState(true);
+    const domRef = useRef();
+
+    const images = [ 
+        personalImg,
+        stella,
+        sf,
+        crochet
+    ]
+
+    const nextSlide = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    };
+    
+    const prevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+        prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    )};
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => setVisible(entry.isIntersecting));
+        });
+        observer.observe(domRef.current);
+        return () => observer.unobserve(domRef.current);
+    }, []);
+    
     return (
         <div id="main-page-container">
             <nav>
@@ -27,16 +60,16 @@ const Main = () => {
             </nav>
             <header className='mb-4'>
                 <div className='intro-section text-white d-flex flex-column align-items-center'>
-                    <div>
+                    <div className='introduction-container'>
                         <h1 className="hello-world">Hello, world!<img src={waveEmoji} alt='wave emoji' className='emoji mx-2' /></h1>
                         <h1 className='name'>I'm Rheanne Bande </h1>
                         <h1 className='intro-3rd-row mb-4' style={{color: "#28104E"}}>I am a <span className='gradient-text'>Software Developer</span></h1>
-                        <p className='small-bio mb-4'>Healthcare transitioned Software Developer based in Bay Area, CA specializing in Front End Web Development</p>
+                        <p className='small-bio mb-4'>Healthcare transitioned Software Developer based in Bay Area, CA</p>
                         <div className='d-flex justify-content-center align-items-center mb-4'>
                             <button className='resume-btn'>Resume</button>
                         </div>
                     </div>
-                    <div>
+                    <div className='introduction-container'>
                         <ul className='d-flex flex-row p-3 mb-5'>
                             <li><Link to={"https://www.linkedin.com/in/rheanne-bande/"} target='_blank' className='text-decoration-none p-3'><i class="devicon-linkedin-plain"></i></Link></li>
                             <li><Link to={"https://github.com/Rhemb"} target='_blank' className='text-decoration-none p-3'><i class="devicon-github-original"></i></Link></li>
@@ -59,15 +92,15 @@ const Main = () => {
                 </div>
             </header>
             <h1 className='section-title text-white mx-4 mb-4' id='about'>About</h1>
-            <section className="about-section">
+            <section className='about-section'>
                 <div className="about-left">
                     <div className='about-paragraph'>
-                        <p>Hello! My name is Rheanne and I enjoy building websites to showcase to others! I'm a healthcare turned software engineer after deciding to try programming through various online resources.</p>
-                        <p>First attending Coding Dojo, a fullstack software engineering bootcamp, helped guide me with the structure and resources to kickstart my career in tech. Navigating my way to learning new languages and technologies has been a challenging yet-rewarding experience!</p>
-                        <p>When I'm not in front of a computer, I enjoy being outdoors, bowling, gaming, or spending time with my friends, family, cats and reptile.</p>
+                        <p>Hi, My name is Rheanne (re-ann)! I'm a healthcare turned software engineer who decided to try programming through various online resources where I found my passion of coding.</p>
+                        <p>The day I decided to make the switch, I attended Coding Dojo, an intensive fullstack software engineering bootcamp, where they provided me the structure and resources to kickstart my career in tech. Navigating my way to learning new languages and technologies has been a challenging yet-rewarding experience!</p>
+                        <p>Up to today, I have been continuing to learn new technologies alongside strengthening my skills. When I'm not in front of a computer I enjoy being outdoors, bowling, gaming, or spending time with my friends, family, and pets!</p>
                     </div>
                     <div className='d-flex flex-column align-items-center'>
-                        <p className='tech-header'>Technologies I have worked with</p>
+                        <p className='tech-header'>Technologies I Have Worked With</p>
                         <div className='tech-list d-flex flex-row'>
                             <ul>
                                 <li>- Javascript </li>
@@ -88,9 +121,22 @@ const Main = () => {
                         </div>
                     </div>
                 </div>
-                <div className="about-right">
-                    <img src={personalImg} alt="personal image" className="personal-img" />
-                </div>
+                {/* <div className="peronsal-img"> */}
+                    {/* <img src={personalImg} alt="personal image" className="personal-img" /> */}
+                    <div className="image-carousel">
+                        <button onClick={prevSlide} className="carousel-button prev">
+                        &lt;
+                        </button>
+                        <img
+                            src={images[currentIndex]}
+                            alt={`Image ${currentIndex + 1}`}
+                            className="carousel-image"
+                        />
+                        <button onClick={nextSlide} className="carousel-button next">
+                            &gt;
+                        </button>
+                    </div>
+                {/* </div> */}
             </section>
             <h1 className='section-title text-white mx-4' id="projects">Projects</h1>
             <section id="project1">
@@ -131,7 +177,7 @@ const Main = () => {
                     </div>
                 </div>
             </section>
-            <section className='contact-section' id='contact'>
+            <section className={`contact-section fade-in-section ${isVisible ? 'is-visible' : ''}`} ref={domRef} id='contact'>
                 <h1 className='contact-header d-flex align-items-center mb-3'>Lets Chat<img src={chatBubble} alt="chat bubble" className="chat-emoji" /></h1>
                 <p className='contact-text'>Thanks for taking the time to get to know me! Please feel free to reach out through my socials if you're interested in connecting</p>
                 <div className='d-flex flex-row'>
